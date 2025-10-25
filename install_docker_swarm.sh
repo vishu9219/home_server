642b3530499c55d0cb2167e99f8a63343d5b33ee
Vishal Srivastava <vishu9219@users.noreply.github.com>
2025-10-26 01:58:54 +0530

    Add script to install and configure Docker Swarm

---
+++ install_docker_swarm.sh
@@ -0,0 +1,39 @@
+# For cluster management
+sudo ufw allow 2377/tcp
+sudo ufw allow 7946/tcp
+sudo ufw allow 7946/udp
+sudo ufw allow 4789/tcp
+sudo ufw allow 4789/udp
+sudo ufw reload
+
+# Package installation
+sudo apt update
+sudo apt upgrade -y
+sudo apt install ca-certificates curl gnupg
+
+# Add Docker's official GPG key
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the Docker repository to Apt sources
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker Engine and components
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# Verify Docker is running
+sudo systemctl status docker
+
+# Add vishal to docker group
+sudo usermod -aG docker vishal
+
+# Init docker swarn
+docker swarm init --advertise-addr <MANAGER_IP>
+
+# Command to run at child cluster
+docker swarm join --token <SWARM_TOKEN> <MANAGER_IP>:2377
